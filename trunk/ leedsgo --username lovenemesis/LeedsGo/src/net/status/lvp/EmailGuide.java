e3c0d24f35b5ebff34604f8e071d26f117780735
lovenemesis <lovenemesis@7b2a633b-fda1-e29a-a54a-2058e4c9c7fc>
2010-07-20 22:20:37 +0000

    Polished email helper (SUCCESS).

--- trunk/ leedsgo --username lovenemesis/LeedsGo/src/net/status/lvp/EmailGuide.java
+++ trunk/ leedsgo --username lovenemesis/LeedsGo/src/net/status/lvp/EmailGuide.java
@@ -2,9 +2,12 @@ package net.status.lvp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.ClipboardManager;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
 import android.content.Intent;
-import android.util.Log;
 
 
 public class EmailGuide extends Activity {
@@ -13,10 +16,12 @@ public class EmailGuide extends Activity {
 	private WebView browser = null;
 	private String username;
 	private String client;
+	private Button buttPer = null;
+	private Button buttEmail = null;
+	private Button buttIMAP = null;
 	private static final String ASSET_EMAIL_ANDROID = "file:///android_asset/email_android.html";
 	private static final String ASSET_EMAIL_K9 = "file:///android_asset/email_k9.html";
 	private static final String ASSET_EMAIL_HTC = "file:///android_asset/email_htc.html";
-	private static final String LOG_TAG = "EmailGuide";
 	
 	/** Called when the activity is first created. */
     @Override
@@ -28,26 +33,63 @@ public class EmailGuide extends Activity {
 
         browser = (WebView)findViewById(R.id.WebView);
         browser.getSettings().setJavaScriptEnabled(true);
+        buttPer = (Button)findViewById(R.id.button_personalize);
+        buttEmail = (Button)findViewById(R.id.button_copy_email);
+        buttIMAP = (Button)findViewById(R.id.button_copy_imap);
         receipt = getIntent();
         username = receipt.getStringExtra("username");
         client = receipt.getStringExtra("client");
-        Log.d(LOG_TAG, "Retrived from intents.");
+        
+        
         //Load different local html files according to clients.
         if (client.equalsIgnoreCase(getString(R.string.email_android))){
         	browser.loadUrl(ASSET_EMAIL_ANDROID);
-        	Log.d(LOG_TAG, "Load android html.");
         }
-        
         if (client.equalsIgnoreCase(getString(R.string.email_k9))){
         	browser.loadUrl(ASSET_EMAIL_K9);
-        	Log.d(LOG_TAG, "Load K9 html.");
-        }
-        
+        }       
         if (client.equalsIgnoreCase(getString(R.string.email_htc))){
         	browser.loadUrl(ASSET_EMAIL_HTC);
-        	Log.d(LOG_TAG, "Load HTC html.");
         }
-        browser.loadUrl("javascript:changeText("+username+")");
-        Log.d(LOG_TAG, "Load javascript.");
+        
+        Toast.makeText(EmailGuide.this, getString(R.string.toast_email_guide), Toast.LENGTH_SHORT)
+		.show();
+
+        
+        buttEmail.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				String email = username +"@leeds.ac.uk";
+				((ClipboardManager)getSystemService(CLIPBOARD_SERVICE)).setText(email);
+		        Toast.makeText(EmailGuide.this, getString(R.string.toast_email_email)+email, Toast.LENGTH_SHORT)
+				.show();
+			}
+		});
+        
+        buttPer.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+		        browser.loadUrl("javascript:changeText('" + username + "')");
+		        Toast.makeText(EmailGuide.this, getString(R.string.toast_email_per), Toast.LENGTH_SHORT)
+				.show();
+			}
+		});
+        
+        buttIMAP.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				String imap = username +".imap.leeds.ac.uk";
+				((ClipboardManager)getSystemService(CLIPBOARD_SERVICE)).setText(imap);
+		        Toast.makeText(EmailGuide.this, getString(R.string.toast_email_imap)+imap, Toast.LENGTH_SHORT)
+				.show();
+			}
+		});
+        
     }
 }
